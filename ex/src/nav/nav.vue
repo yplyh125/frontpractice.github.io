684d9ecd78957900f09531d9857d86a7afe7359f
yeeun <yeeun.han@snetsystems.co.kr>
2024-02-23 17:25:58 +0900

    연습

--- ex/src/nav/nav.vue
+++ ex/src/nav/nav.vue
@@ -6,6 +6,7 @@
       <div class="mt-4 text-sm font-bold">
         Your additional content can be added with default slot, You may put as
         many content as you want here.
+        aaa
       </div>
     </el-page-header>
 </template>
